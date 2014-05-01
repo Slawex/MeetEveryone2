f5bfba33b4c2eef9b1116cfb9039222244bbf1a1
Slawex <slawomirbukowiec@gmail.com>
2014-05-01 21:25:52 +0200

    Changes to Contact class, added LoginActivity and activity for RegistrationActivity

--- Meet Everywhere/src/com/meetEverywhere/Contact.java
+++ Meet Everywhere/src/com/meetEverywhere/Contact.java
@@ -2,18 +2,11 @@ package com.meetEverywhere;
 
 public class Contact {
 
-	private int id;
 	private String name;
-	private String number;
+	private String token;
 	
-	public Contact(int id, String name, String number){
-		this.setId(id);
+	public Contact(String name){
 		this.name = name;
-		this.number = number;
-	}
-	
-	public Contact(String name, String num){
-		this(-1, name, num);
 	}
 	
 	public String getName() {
@@ -24,25 +17,4 @@ public class Contact {
 		this.name = name;
 	}
 	
-	public String getNumber() {
-		return number;
-	}
-	
-	public void setNumber(String number) {
-		this.number = number;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-	
-	@Override
-	public boolean equals(Object o2){
-		Contact c2 = (Contact) o2;
-		return name.equals(c2.name) && number.equals(c2.number);
-	}
 }
