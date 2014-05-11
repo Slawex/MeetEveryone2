e4994a5f68ce6ae3a1c7333c0b210916946c4844
marcin <marbogster@gmail.com>
2014-05-11 22:00:14 +0200

    Drobna zmiana w widoku tagow (chyba juz ostateczna). Zaznaczanie checkboxa przez klikniecie na napis kolo niego

--- Meet Everywhere/src/com/meetEverywhere/MyCustomAdapter.java
+++ Meet Everywhere/src/com/meetEverywhere/MyCustomAdapter.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MyCustomAdapter extends ArrayAdapter<Contact> {
@@ -26,8 +27,13 @@ public class MyCustomAdapter extends ArrayAdapter<Contact> {
   }
  
   private class ViewHolder {
-   TextView code;
-   CheckBox name;
+	   TextView code;
+	   CheckBox name;
+	   LinearLayout row;
+  }
+  
+  public void changePosition(int position){
+	  countryList.get(position).toggle();
   }
  
   @Override
@@ -44,7 +50,11 @@ public class MyCustomAdapter extends ArrayAdapter<Contact> {
    holder = new ViewHolder();
    holder.code = (TextView) convertView.findViewById(R.id.tag_name);
    holder.name = (CheckBox) convertView.findViewById(R.id.checkBox1);
+   holder.row = (LinearLayout) convertView.findViewById(R.id.row);
    convertView.setTag(holder);
+   
+   
+  
  
     holder.name.setOnClickListener( new View.OnClickListener() { 
      public void onClick(View v) { 
@@ -58,9 +68,11 @@ public class MyCustomAdapter extends ArrayAdapter<Contact> {
     	  country.setChecked(cb.isChecked());
      } 
     }); 
+    
+    
    }
    else {
-    holder = (ViewHolder) convertView.getTag();
+	   holder = (ViewHolder) convertView.getTag();
    }
  
    if(countryList.size()>0){
