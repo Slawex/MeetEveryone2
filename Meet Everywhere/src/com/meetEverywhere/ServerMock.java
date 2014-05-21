a76d36024fd6382ddc82f894fb018526af7264e3
marekmagik <marekmagik1@gmail.com>
2014-05-21 10:55:09 +0200

    Usunięte zbędne klasy.

--- Meet Everywhere/src/com/meetEverywhere/ServerMock.java
+++
@@ -1,9 +0,0 @@
-package com.meetEverywhere;
-
-/**
- * Klasa bêdzie zawiera³a metody, do których odwo³a siê DAO, na potrzeby
- * debuggowania.
- */
-public class ServerMock {
-
-}
--- Meet Everywhere/src/com/meetEverywhere/Status.java
+++
@@ -1,5 +0,0 @@
-package com.meetEverywhere;
-
-public enum Status {
-	SAVED, IN_EDITION, EDITED
-}
